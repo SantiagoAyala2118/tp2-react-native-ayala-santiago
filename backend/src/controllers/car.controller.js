649708d3b21a77f538c63af52dd7b6c9154cc912
Santiago Ayala <santitomayala07@gmail.com>
2026-03-30 20:09:26 -0300

    Nombre de las funciones cambiados e importacion del modelo a utilizar en las operaciones CRUD

---
+++ backend/src/controllers/car.controller.js
@@ -0,0 +1,13 @@
+//? ----------------------------------------------Importaciones
+import { carModel } from "../models/car.model.js";
+
+//? ---------------------------------------------- Controladores
+export const createCar = async (req, res) => {};
+
+export const getCars = async (req, res) => {};
+
+export const getCarById = async (req, res) => {};
+
+export const updateCar = async (req, res) => {};
+
+export const removeCar = async (req, res) => {};
