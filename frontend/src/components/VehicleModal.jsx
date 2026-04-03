bc3fffa124f82d0615f64ada1329f8b978a1a9d4
Santiago Ayala <santitomayala07@gmail.com>
2026-04-03 16:44:43 -0300

    Se cambio la logica para que al mostrar el modal se haga una peticion a un endpoint del backend y se implemento un componente de loading nuevo

--- frontend/src/components/VehicleModal.jsx
+++ frontend/src/components/VehicleModal.jsx
@@ -1,9 +1,12 @@
-// components/VehicleModal.jsx
 import { useState, useEffect } from "react";
+import ModalLoading from "./ModalLoading";
 
 const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
   const [imgIdx, setImgIdx] = useState(0);
   const imagenes = vehicle.imagenes || [];
+
+  const [vehicleData, setVehicleData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   // Cerrar con Escape
   useEffect(() => {
@@ -19,10 +22,35 @@ const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
     setImgIdx((prev) => (prev + dir + imagenes.length) % imagenes.length);
   };
 
+  useEffect(() => {
+    const getCarById = async () => {
+      try {
+        setLoading(true);
+
+        const res = await fetch(`http://localhost:3000/api/car/${vehicle.id}`);
+        const data = await res.json();
+
+        setVehicleData(data.data);
+      } catch (error) {
+        console.error("Error al traer el auto:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getCarById();
+  }, [vehicle.id]);
+
+  if (loading || !vehicleData) {
+    return <ModalLoading />;
+  }
+
   return (
     <div
       className="fixed inset-0 z-50 flex items-center justify-center"
-      style={{ background: "rgba(6,20,27,0.92)", backdropFilter: "blur(6px)" }}
+      style={{
+        background: "rgba(6,20,27,0.92)",
+        backdropFilter: "blur(6px)",
+      }}
       onClick={(e) => {
         if (e.target === e.currentTarget) onClose();
       }}
@@ -36,7 +64,7 @@ const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
           {imagenes.length > 0 ? (
             <img
               src={imagenes[imgIdx]}
-              alt={`${vehicle.marca} ${vehicle.modelo}`}
+              alt={`${vehicleData.marca} ${vehicleData.modelo}`}
               className="w-full h-full object-cover"
             />
           ) : (
@@ -117,7 +145,7 @@ const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
         {/* Cuerpo */}
         <div className="p-6">
           <p className="text-[10px] tracking-[2.5px] uppercase text-[#4A5C6A]">
-            {vehicle.marca}
+            {vehicleData.marca}
           </p>
           <p
             className="text-[#CCD0CF] leading-none tracking-wide mb-3"
@@ -126,18 +154,18 @@ const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
               fontSize: "2.2rem",
             }}
           >
-            {vehicle.modelo}
+            {vehicleData.modelo}
           </p>
 
           {/* Stats */}
           <div className="grid grid-cols-3 gap-3 mb-4">
             {[
-              { label: "Año", val: vehicle.anio },
+              { label: "Año", val: vehicleData.anio },
               {
                 label: "Km",
-                val: `${vehicle.kilometraje?.toLocaleString("es-AR")} km`,
+                val: `${vehicleData.kilometraje?.toLocaleString("es-AR")} km`,
               },
-              { label: "Estado", val: vehicle.estado },
+              { label: "Estado", val: vehicleData.estado },
             ].map(({ label, val }) => (
               <div
                 key={label}
@@ -161,7 +189,7 @@ const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
 
           {/* Descripción */}
           <p className="text-xs text-[#4A5C6A] leading-relaxed italic mb-5">
-            {vehicle.descripcion}
+            {vehicleData.descripcion}
           </p>
 
           {/* Precio + acciones */}
@@ -178,13 +206,13 @@ const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
                 className="text-[#CCD0CF] tracking-wide"
               >
                 <span className="text-[#4A5C6A] text-base">USD </span>
-                {vehicle.precio?.toLocaleString("es-AR")}
+                {vehicleData.precio?.toLocaleString("es-AR")}
               </p>
             </div>
             <div className="flex gap-2">
               <button
                 onClick={() => {
-                  onEdit?.(vehicle);
+                  onEdit?.(vehicleData);
                   onClose();
                 }}
                 className="text-xs px-4 py-2 rounded-lg bg-[#1E3040] text-[#9BA8AB] border border-[#253745] hover:opacity-75 transition-opacity"
@@ -193,7 +221,7 @@ const VehicleModal = ({ vehicle, onClose, onEdit, onDelete }) => {
               </button>
               <button
                 onClick={() => {
-                  onDelete?.(vehicle.id);
+                  onDelete?.(vehicleData.id);
                   onClose();
                 }}
                 className="text-xs px-4 py-2 rounded-lg bg-[#2d1010] text-[#c45a5a] border border-[#4a1f1f] hover:opacity-75 transition-opacity"
