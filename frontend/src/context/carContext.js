f59acdf005e8bdcca999d6e9828adf53a8f6e984
Santiago Ayala <santitomayala07@gmail.com>
2026-04-27 10:24:00 -0300

    Se agregaron los hooks nativos useContext y useReducer para el manejo de estados de la aplicacion

---
+++ frontend/src/context/carContext.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useReducer, useEffect } from "react";
+import { carReducer } from "./carReducer";
+
+// 1. Estado inicial
+const initialState = {
+  cars: [],
+  loading: false,
+  error: false,
+  showForm: false,
+  vehicleToEdit: null,
+};
+
+//* Contexto
+const CarContext = createContext();
+
+//* Porvider
+export const CarProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(carReducer, initialState);
+
+  //* Pide los autos del backend
+  const getCars = async () => {
+    dispatch({ type: "SET_LOADING", payload: true });
+    try {
+      const response = await fetch("http://localhost:3000/api/cars");
+      const data = await response.json();
+      await new Promise((resolve) => setTimeout(resolve, 700));
+      dispatch({ type: "SET_CARS", payload: data.data });
+    } catch (err) {
+      dispatch({ type: "SET_ERROR", payload: true });
+      console.error("Error al traer los autos:", err);
+    } finally {
+      dispatch({ type: "SET_LOADING", payload: false });
+    }
+  };
+
+  useEffect(() => {
+    getCars();
+  }, []);
+
+  //* Todo lo que queremos compartir con la app
+  const value = {
+    ...state, // cars, loading, error, showForm, vehicleToEdit
+    dispatch, // para despachar acciones
+    getCars, // para recargar la lista
+  };
+
+  return <CarContext.Provider value={value}>{chilsdren}</CarContext.Provider>;
+};
+
+// 5. Hook personalizado para usar el contexto
+export const useCars = () => useContext(CarContext);
---
+++ frontend/src/context/carReducer.js
@@ -0,0 +1,44 @@
+export const carReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_CARS":
+      //* Traer los autos del backend
+      return { ...state, cars: action.payload };
+
+    case "ADD_CAR":
+      //* Agregar un auto
+      return { ...state, cars: [...state.cars, action.payload] };
+
+    case "EDIT_CAR":
+      //* Busca el auto por id y lo reemplaza con los datos nuevos
+      return {
+        ...state,
+        cars: state.cars.map((car) => {
+          car.id === action.payload ? action.payload : car;
+        }),
+      };
+
+    case "DELETE_CAR":
+      //* Filtra por id y quita el auto
+      return {
+        ...state,
+        cars: state.cars.filter((car) => {
+          car.id !== action.payload;
+        }),
+      };
+
+    case "SET_LOADING":
+      return { ...state, loading: action.payload };
+
+    case "SET_ERROR":
+      return { ...state, error: action.payload };
+
+    case "SET_SHOW_FORM":
+      return { ...state, showForm: action.payload };
+
+    case "SET_VEHICLE_TO_EDIT":
+      return { ...state, vehicleToEdit: action.payload };
+
+    default:
+      return state;
+  }
+};
